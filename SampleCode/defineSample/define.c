2112a1af410fbe550adfb81299e38dd7eee892dd
haradariamu <116865013+haradariamu@users.noreply.github.com>
2023-11-02 14:53:21 +0900

    Update define.c

---
+++ SampleCode/defineSample/define.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+#define PI 3.14f
+#define CIRCLE_RATIO "円周率"
+ 
+int main(void) {
+    double area = PI * 10 * 10;
+    char *ptr = CIRCLE_RATIO;
+    
+    printf("%sは: %f\n", ptr, PI);
+    printf("半径10の円の面積は: %f\n", area);
+    
+    return 0;
+}
