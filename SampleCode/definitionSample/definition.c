9b6acf7d2031d2d042e7f1e70b0896df1acfdf21
haradariamu <116865013+haradariamu@users.noreply.github.com>
2023-11-02 14:41:10 +0900

    変数追加

---
+++ SampleCode/definitionSample/definition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    int a;
+    int b = 0;
+    float c;
+    float d = 0.0;
+    double e;
+    double f = 0.0;
+    char g;
+    char h = 'a';
+
+    return 0;
+}
